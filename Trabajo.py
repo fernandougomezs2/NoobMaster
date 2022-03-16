7e026b16eb12be76a71f874c9bb5478594f0be77
AustinklB <austinklbrown1@gmail.com>
2022-03-16 07:33:10 -0600

    Austin estuvo aqui

--- Trabajo.py
+++ Trabajo.py
@@ -1,9 +1,30 @@
 print("---MENU---")
-print("1.- ")
-print("2.- ")
+print("1.-sacar el area de un circulo")
+print("2.- numeros de fibonacci ")
 
+sigma = int(input("ingresa la funcion requerida"))
+
+if sigma == int(1):
 # Austin
+    pi1 = float(3.1416)
+    rad1 = int()
+    res = int()
+    resu = float()
 
+
+    rad1 = int(input("Buenas tardes, digame el radio de su ciculo."))
+
+    res = int(input("deseas calcular el area del ciculo 1. para si 0. para cancelar"))
+
+
+    if res > int(0):
+        resu = pi1 * rad1**2
+        print(resu, "es el area de su circulo.")
+    else:
+        res == int(1)
+        res = int(input("disculpe el error, vulve a introducir el pi"))
+        resu = pi1 * rad1^2
+        print(resu, "es el perimetro de su circulo.")
 
 
 
