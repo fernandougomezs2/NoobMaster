ed3eee576864025d4b1351b122659ef30af03ba5
Alfex122 <90349759+Alfex122@users.noreply.github.com>
2022-03-16 07:59:36 -0600

    Numero Fibonacci

--- Trabajo.py
+++ Trabajo.py
@@ -11,3 +11,23 @@ print("2.- ")
 
 
 # Alexis
+elif sigma == 2:
+	print("Ingresar el rango de numeros que se desea obtener en secuencia fibonacci")
+	Heidis = int(input("Rango: "))
+
+
+	def Fibonacci(n):
+		Alexis = 0
+		Austin = 1
+
+		for x in range(n):
+			Fernando = Alexis + Austin
+			Alexis = Austin
+			Austin = Fernando
+		return Austin
+
+	for Ara in range(Heidis):
+		print(Fibonacci(Ara))
+
+else:
+	print("Error")
